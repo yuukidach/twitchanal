a61bce0f16759e074aa3fa0e265169092ae0f6a5
Lu Sheng <stephen@Lus-MacBook-Pro.local>
2020-11-11 00:18:06 +0800

    finish CalcSumViewer

---
+++ twitchanal/src/twitchanal/datacollection/CalcSumViewer.py
@@ -0,0 +1,40 @@
+### This function calculate sum of viewer of each game
+import numpy as np 
+import pandas as pd
+import os
+
+# This function calculate sum of viewer for each game
+def TotalGameView(path):
+    # @path: directory of top_game_csv file
+    
+    #read data and add sum column at the end
+    games = pd.read_csv(path)
+    games['Total_Viewer'] = np.nan
+    
+    #sort games according to names
+    games_sorted = games.sort_values('name')
+    
+    nb_of_game = len(games)
+    
+    path_of_streamer = os.path.dirname(path)+'/game_streams'
+    
+    streams = sorted(find_csv_filenames(path_of_streamer, suffix=".csv"))
+    
+    # calculate sum using loop
+    total = []
+    for i in range(0, nb_of_game):
+        game = pd.read_csv(path_of_streamer+'/'+streams[i])
+        total_view = game['viewer_count'].sum()
+        total.append(total_view)
+        
+    games_sorted['Total_Viewer'] = total
+    result = games_sorted.sort_values('Total_Viewer', ascending=False)
+
+    
+    return result
+    
+
+# return a list of sorted csv file name
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = os.listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
